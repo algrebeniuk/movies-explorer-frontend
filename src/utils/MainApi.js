9743b7a8dc43fe693892dda0c3e7f230c9403891
Alexander Grebeniuk <al.al.grebeniuk@gmail.com>
2023-05-17 18:07:12 +0300

    изменена ссылка на api

--- src/utils/MainApi.js
+++ src/utils/MainApi.js
@@ -1,5 +1,5 @@
-export const url = //'https://api.algrebeniuk.diplom.nomoredomains.work'; 
-'http://localhost:3002';
+export const url = 'https://api.algrebeniuk.diplom.nomoredomains.work'; 
+//'http://localhost:3002';
 
 
 class MainApi {
